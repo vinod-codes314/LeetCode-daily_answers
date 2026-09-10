83f7dc5be082a843d7a39725a74d7e65494ed1d2
NUKASANI VENKATA VINOD <vinodnukasani58@gmail.com>
2026-09-10 22:39:58 +0530

    LeetCode2265->10-09-26->Answer

---
+++ LeetCode2265-answer.py
@@ -0,0 +1,21 @@
+class Solution:
+    def averageOfSubtree(self, root):
+        self.count = 0
+
+        def trav(node):
+            if node is None:
+                return (0, 0)
+
+            leftSum, leftCount = trav(node.left)
+            rightSum, rightCount = trav(node.right)
+
+            subtreeSum = leftSum + rightSum + node.val
+            subtreeCount = leftCount + rightCount + 1
+
+            if subtreeSum // subtreeCount == node.val:
+                self.count += 1
+
+            return (subtreeSum, subtreeCount)
+
+        trav(root)
+        return self.count
